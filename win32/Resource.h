7d469838a8939bc80bf8e3d4d11f525af7ab18e0
Nathan Campos <nathan@innoveworkshop.com>
2022-08-05 20:25:32 +0100

    Built the beginnings of the Windows detail view

--- win32/Resource.h
+++ win32/Resource.h
@@ -3,7 +3,7 @@
 // Used by Pecan.rc
 //
 #define IDC_MYICON                      2
-#define IDD_PECAN_DIALOG                102
+#define IDD_DETAILVIEW                  9
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
@@ -11,7 +11,12 @@
 #define IDI_PECAN                       107
 #define IDI_SMALL                       108
 #define IDC_PECAN                       109
-#define IDR_MAINFRAME                   128
+#define IDC_IMAGE                       1000
+#define IDC_EDIT_QUANTITY               1001
+#define IDC_EDIT_DESCRIPTION            1002
+#define IDC_EDIT_NAME                   1003
+#define IDC_COMBO_PACKAGE               1004
+#define IDC_LIST_ATTRS                  1005
 #define ID_FILE_OPENARCHIVE             32771
 #define IDM_FILE_OPENARCHIVE            32772
 #define IDC_STATIC                      -1
@@ -21,9 +26,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
